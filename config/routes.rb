7bb65ab1ed6c4f10c5d54c8ea16f66ca57993af1
Joaquin Roca <me@joaquinroca.com>
2013-05-08 00:05:43 -0400

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 SampleApp::Application.routes.draw do
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
   
-  match '/signup',  to: 'users#new'
-  match '/signin',  to: 'sessions#new'
-  match 'signout',  to: 'sessions#destroy', via: :delete
+  match '/signup',   to: 'users#new'
+  match '/signin',   to: 'sessions#new'
+  match '/signout',  to: 'sessions#destroy', via: :delete
   
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
-  match '/extra',   to: 'static_pages#extra'
+  match '/help',     to: 'static_pages#help'
+  match '/about',    to: 'static_pages#about'
+  match '/contact',  to: 'static_pages#contact'
+  match '/extra',    to: 'static_pages#extra'
   
 
   # The priority is based upon order of creation:
